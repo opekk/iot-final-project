2330a65290fdcf101c419fa25da89a405e96ec9c
opekk <maciek09874@gmail.com>
2026-05-16 14:23:57 +0200

    feat: pins define for esp32

---
+++ include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define PWM 13
+
+#define LED_SCL 33
+#define LED_SDA 34
+#define LED_RES 35
+#define LED_DC 36
+#define LED_CS 37
+
+#define DS_DATA 39
+
+#endif
